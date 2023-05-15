acc7e0eb5a265ea95fe218d86549fa7da598c92f
kuhlbeans <kuhlpatch@gmail.com>
2023-05-15 15:01:19 +1000

    login working

--- project/models.py
+++ project/models.py
@@ -1,6 +1,22 @@
 from flask_login import UserMixin
 from . import db
 
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+
+    @property
+    def serialize(self):
+       """Return object data in easily serializeable format"""
+       return {
+           'id'         : self.id,
+           'email'      : self.email,
+           'password'   :self.password,
+           'name'       :self.name,
+       }
+
 class Restaurant(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), nullable=False)
@@ -12,12 +28,6 @@ class Restaurant(db.Model):
            'name'         : self.name,
            'id'           : self.id,
        }
-    
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
-    name = db.Column(db.String(1000))
 
  
 class MenuItem(db.Model):
