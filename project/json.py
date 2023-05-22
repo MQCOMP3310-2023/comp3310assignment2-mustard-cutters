9d9fb0e8e83639d649d3a70524614194c5f6b829
Craig Thomas <craig.thomas@students.mq.edu.au>
2023-05-22 14:05:30 +1000

    Fixed Remaing SQL Injection Vuln

--- project/json.py
+++ project/json.py
@@ -9,15 +9,16 @@ json = Blueprint('json', __name__)
 #JSON APIs to view Restaurant Information
 @json.route('/restaurant/<restaurant_id>/menu/JSON')
 def restaurantMenuJSON(restaurant_id):
-    items = db.session.execute(text('select * from menu_item where restaurant_id = ' + str(restaurant_id)))
-    items_list = [ i._asdict() for i in items ]
+    query = 'SELECT * FROM menu_item WHERE restaurant_id = :restaurant_id'
+    items = db.session.execute(text(query), {"restaurant_id": restaurant_id})
+    items_list = [i._asdict() for i in items]
     return pyjs.dumps(items_list)
 
 
 @json.route('/restaurant/<restaurant_id>/menu/<int:menu_id>/JSON')
 def menuItemJSON(restaurant_id, menu_id):
-    menu_item = "SELECT * FROM menu_item WHERE id = %s LIMIT 1"
-    menu_item.execute(menu_item, (menu_id,))
+    query = "SELECT * FROM menu_item WHERE id = %s LIMIT 1"
+    menu_item = db.session.execute(text(query), {"menu_id", menu_id,})
     items_list = [ i._asdict() for i in menu_item ]
     return pyjs.dumps(items_list)
 
